17ce2b31f7bc11ad43e2824408a1e38a952efb08
delm959 <dan_enz@hotmail.com>
2016-08-19 12:19:11 +1200

    Changed animation variable name

---
+++ app/src/main/java/com/authorwjf/deal/Transitions.java
@@ -0,0 +1,18 @@
+package com.authorwjf.deal;
+
+import android.graphics.drawable.TransitionDrawable;
+
+public class Transitions {
+	
+	TransitionDrawable transition;
+	
+	
+	public TransitionDrawable getTransition() {
+		return transition;
+	}
+
+	public void setTransition(TransitionDrawable transition) {
+		this.transition = transition;
+	}
+
+}
